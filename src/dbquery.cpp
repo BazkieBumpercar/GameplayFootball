31e3d04ada72917409a932d5986d16bb6e6b2879
Bastiaan Schuiling <b.schuiling@properlydecent.com>
2017-11-06 13:52:52 +0100

    corrected dates

--- src/dbquery.cpp
+++ src/dbquery.cpp
@@ -1,6 +1,5 @@
-// written by bastiaan konings schuiling 2008 - 2014
+// written by bastiaan konings schuiling 2008 - 2015
 // this work is public domain. the code is undocumented, scruffy, untested, and should generally not be used for anything important.
 // i do not offer support, so don't ask. to be used for inspiration :)
 
 #include "dbquery.hpp"
-
